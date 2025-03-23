8d2026f792e72ebc3bca931caad3233a56191a3c
ValDrayn <evaldoraynardi28@gmail.com>
2025-03-23 23:26:14 +0700

    Revert "Finalize FE Project AI"
    This reverts commit c1e4861a5cc5094dcfbd45021b6f42037458ee42.

--- src/components/ui/HeaderProfile.tsx
+++ src/components/ui/HeaderProfile.tsx
@@ -1,23 +1,7 @@
-import { useEffect, useState } from "react";
 import logo from "/TrackerLogo.png"
-import { cn } from "../../lib/utils";
 
 export default function HeaderProfile() {
-    const [isHeight, setIsHeight] = useState<number>(window.innerHeight);
-
-    const smallDevice = 720;
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsHeight(window.innerHeight);
-        }
-
-        handleResize();
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-    }, [])
-
-    return <div className={cn(`flex flex-col justify-center items-center w-full ${isHeight < smallDevice ? "scale-90" : "scale-95 mb-3" }`)}>
+    return <div className=" flex flex-col justify-center items-center w-full mb-3 scale-95">
         <img src={logo} alt="Logo" />
         <p className="font-header text-[4rem] tracking-wider leading-none" style={{color: "#80A217"}}>TRACKER</p>
     </div>;
